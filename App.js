a804cc24e396565371c2817e575f60a3c9fa966c
dawit <dawit42001@gmail.com>
2018-06-13 23:25:26 -0700

    Initial commit

---
+++ App.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { AppRegistry, FlatList, ActivityIndicator, StyleSheet, Button, View, Text } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>React Native App</Text>
+        <Button
+          title="Go to Cameras"
+          onPress={() => this.props.navigation.navigate('Cameras')}
+        />
+      </View>
+    );
+  }
+}
+
+class Cameras extends React.Component {
+  static navigationOptions = {
+    title: 'Cameras',
+  };
+
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: true}
+  }
+
+  componentDidMount(){
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.movies,
+        }, function(){
+
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+  render() {
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
+    return(
+      <View style={{flex: 1, paddingTop:20}}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
+          keyExtractor={(item, index) => index}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Cameras: Cameras,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
